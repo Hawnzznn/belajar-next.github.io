060551b7879449f9edb3c41571b4b622e30f1106
hawa <hawanf1234@gmail.com>
2023-11-03 12:53:10 +0700

    style

--- next-ki/pages/components/profil.js
+++ next-ki/pages/components/profil.js
@@ -1,4 +1,10 @@
 const profil = () => {
-    return <>profil</>;
-}
+    return (
+        <>
+        <navbar/>
+        <h1>ini halaman profil</h1>
+        </>
+    );
+
+};
 export default profil;
