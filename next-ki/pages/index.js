fc659f534d076d6016b2f50aa8b3f4901f3b931b
hawa <hawanf1234@gmail.com>
2023-11-03 13:51:07 +0700

    style

--- next-ki/pages/index.js
+++ next-ki/pages/index.js
@@ -1,9 +1,10 @@
-import navbar from '../components/navbar'
+import Navbar from './components/navbar'
+
 export default function Home() {
   return (
     <>
-      <navbar/>
-      <h1>ini halaman Home</h1>
+      <Navbar/>
+      <h1>Ini halaman Home</h1>
     </>
   );
 }
